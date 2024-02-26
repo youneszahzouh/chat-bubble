a1b0ef09f08e9bbc956869ef94fe4f3ba5218db8
Younes Zahzouh <youneszahzouhitexc@gmail.com>
2024-02-26 23:39:09 +0100

    feat: show list of Messages + send new Message

--- src/components/chatBubble/ChatWindow.tsx
+++ src/components/chatBubble/ChatWindow.tsx
@@ -1,14 +1,77 @@
+import { KeyboardEvent, useCallback, useState } from "react";
 import { MessageItem } from "./MessageItem";
-import { messages } from "./mockData";
+import { IMessage, defaultMessage, localMessages } from "./mockData";
+import { cn } from "../../utils/cn";
+import { GalleryExport, Microphone, Send } from "iconsax-react";
 
 const ChatWindow = () => {
+  const [messages, setMessages] = useState(localMessages);
+
+  const onSendMessage = (message: IMessage) => {
+    setMessages([...messages, message]);
+  };
   return (
-    <div className="flex flex-col w-full bg-slate-100 rounded-lg p-4 border-slate-200">
+    <div className="flex flex-col  gap-4 w-full bg-gray-800 rounded-lg p-4 border-gray-200">
       <div className="flex flex-col gap-2">
-        {messages?.map((message) => (
-          <MessageItem data={message} />
+        {messages?.map((message, index) => (
+          <MessageItem key={index} data={message} />
         ))}
       </div>
+
+      <MessagingInputs onSendMessage={onSendMessage} />
+    </div>
+  );
+};
+
+export const MessagingInputs = ({ onSendMessage }) => {
+  const [message, setMessage] = useState<IMessage>(defaultMessage);
+
+  const handleSendMessage = useCallback(
+    (message: IMessage) => {
+      onSendMessage(message);
+      setMessage({
+        ...message,
+        message: { ...message.message, content: "" },
+      });
+    },
+    [onSendMessage]
+  );
+
+  const onKeyUp = useCallback(
+    (event: KeyboardEvent<HTMLTextAreaElement>): void => {
+      if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault();
+
+        handleSendMessage(message);
+      }
+    },
+    [handleSendMessage, message]
+  );
+
+  return (
+    <div className="flex gap-1">
+      <div className="flex gap-2">
+        <Microphone size="32" variant="Bold" color="#ED3C3A" />
+        <GalleryExport size="32" variant="Bold" color="#ED3C3A" />
+      </div>
+
+      <textarea
+        className={cn(
+          "resize-none rounded-full max-h-16  flex w-full  bg-gray-700 text-white px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-none focus-visible:ring-none disabled:cursor-not-allowed disabled:opacity-50"
+        )}
+        rows={message.message.content.split("\n").length || 1}
+        value={message.message.content}
+        onChange={(e) => {
+          setMessage({
+            ...message,
+            message: { ...message.message, content: e.target.value },
+          });
+        }}
+        onKeyUp={onKeyUp}
+      />
+      <button onClick={() => handleSendMessage(message)}>
+        <Send size="32" variant="Bold" color="#ED3C3A" />
+      </button>
     </div>
   );
 };
--- src/components/chatBubble/mockData.ts
+++ src/components/chatBubble/mockData.ts
@@ -1,19 +1,19 @@
+export type TMessageContentType = "TEXT" | "IMAGE" | "VOICE";
+
 export interface IMessage {
-    user: {
-      name: string;
-      avatarUrl: string;
-      isMe: boolean;
-    };
-    message: {
-      contentType: "TEXT" | "IMAGE" | "VOICE";
-      content: string;
-    };
-    createdAt: string;
-  }
-  
+  user: {
+    name: string;
+    avatarUrl: string;
+    isMe: boolean;
+  };
+  message: {
+    contentType: TMessageContentType;
+    content: string;
+  };
+  createdAt: string;
+}
 
-  
-export const messages: Array<IMessage> = [
+export const localMessages: Array<IMessage> = [
   {
     user: {
       name: "Younes",
@@ -22,20 +22,7 @@ export const messages: Array<IMessage> = [
     },
     message: {
       contentType: "TEXT",
-      content: "Hey Younes, How are you ?",
-    },
-    createdAt: new Date().toISOString(),
-  },
-
-  {
-    user: {
-      name: "Zahzouh",
-      avatarUrl: "https://api.dicebear.com/7.x/initials/svg?seed=Zahzouh",
-      isMe: true,
-    },
-    message: {
-      contentType: "TEXT",
-      content: "Hey Younes, How are you ?",
+      content: "Test message",
     },
     createdAt: new Date().toISOString(),
   },
@@ -48,8 +35,21 @@ export const messages: Array<IMessage> = [
     },
     message: {
       contentType: "TEXT",
-      content: "Hey Younes, How are you ?",
+      content: "can we have a meet ?",
     },
     createdAt: new Date().toISOString(),
   },
 ];
+
+export const defaultMessage: IMessage = {
+  user: {
+    name: "Zahzouh",
+    avatarUrl: "https://api.dicebear.com/7.x/initials/svg?seed=Zahzouh",
+    isMe: true,
+  },
+  message: {
+    contentType: "TEXT",
+    content: "",
+  },
+  createdAt: new Date().toISOString(),
+};
